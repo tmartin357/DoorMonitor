de239dbc4d53c9fc82a8edcfbc79c0930dce9bbb
Taylor Martin <taylor.martin357@gmail.com>
2013-09-12 11:00:53 -0400

    Add licence to monitor.py

--- monitor.py
+++ monitor.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python2
 
-#Edit the credentials at or near line 64
+#--------------------------------------------------------------------------
+# LICENCE
+#--------------------------------------------------------------------------
+#This monitor.py is part of SPSUAUVDoorMonitor.
+#
+#SPSUAUVDoorMonitor is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#SPSUAUVDoorMonitor is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with SPSUAUVDoorMonitor. If not, see http://www.gnu.org/licenses/.
+#--------------------------------------------------------------------------
+
+#Edit the credentials at or near line 83
  
 import serial
 import datetime
